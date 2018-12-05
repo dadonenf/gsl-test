3037cb8b7b6cea6caa76b1cca3304c9a4f3907d7
Daniel Donenfeld <dadonenf@microsoft.com>
2018-12-05 15:27:11 -0800

    Initial changes to add asm generation build targets

---
+++ tests/no_exception_throw_tests.cpp
@@ -0,0 +1,11 @@
+#ifdef _MSC_VER
+// blanket turn off warnings from CppCoreCheck from catch
+// so people aren't annoyed by them when running the tool.
+#pragma warning(disable : 26440 26426 26497) // from catch
+
+#endif
+
+int main()
+{
+    return 0;
+}
